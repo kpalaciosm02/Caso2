56e3073c822400c58dac56a091ca31d9400d377d
kpalacios <kpalaciosm02@gmail.com>
2021-09-13 17:43:08 -0600

    Logic ready, commentaries are missing.

--- FirstExercise.cpp
+++ FirstExercise.cpp
@@ -2,65 +2,44 @@
 using namespace std;
 
 int getMin(int * list, int size, int firstIndex){
-    //returns the index of the minimal number in an array.
     int min = list[firstIndex];
-    for (int i = firstIndex; i <= size; i++){
-        //cout << "List[i]: " << list[i] << " Min: " << min << endl;
-        if (list[i] < min){
+
+    for (int i = firstIndex; i < size; i++){
+        if (list[i] < min)
             min = list[i];
-        }
     }
-    for (int i = firstIndex; i <= size; i++){
-        if (list[i] == min)
-            return i;
+
+    for (int j = 0; j < size; j++){
+        if (list[j] == min){
+            return j;
+        }
     }
 }
 
-int * updateList(int * listToUpdate, int MinNumberIndex, int firstIndex){
-    int minNumber = listToUpdate[minNumber];
-    int numberFirstIndex = listToUpdate[firstIndex];
-    listToUpdate[firstIndex] = minNumber;
-    listToUpdate[MinNumberIndex] = numberFirstIndex;
-    return listToUpdate;
+int * updateList(int * list, int firstIndex, int minNumberIndex){
+    int minNumber = list[minNumberIndex];
+    int firstIndexNumber = list[firstIndex];
+    list[firstIndex] = minNumber;
+    list[minNumberIndex] = firstIndexNumber;
+    return list;
 }
 
 void sortBySelectionSort(int * pvaloresAOrdenar, int size){
-    int firstIndex = 0;
-    while(firstIndex < size - 1){
-        int minNumberIndex = getMin(pvaloresAOrdenar, size, firstIndex);
-        //cout << pvaloresAOrdenar[minNumberIndex] << endl;
-        pvaloresAOrdenar = updateList(pvaloresAOrdenar,minNumberIndex,firstIndex);
-        firstIndex ++;
+    int index = 0;
+    while (index < size - 1){
+        int minNumberIndex = getMin(pvaloresAOrdenar, size, index);
+        pvaloresAOrdenar = updateList(pvaloresAOrdenar,index,minNumberIndex);
+        index ++;
     }
-    //cout << pvaloresAOrdenar[0];
     for (int i = 0; i < size; i++){
         cout << pvaloresAOrdenar[i] << "->";
     }
+    cout << endl;
 }
 
 int main(){
-    /*int pValores[] = {1,2,3,4,5};
-    int *ptr = pValores;*/
-    //cout << ptr[0];
-    int lista[] = {3,1,4,2,7};
+    int lista[] = {24,42,58,2,104,17,99,13,8,145,59};
     int * ptr = lista;
-    //sortBySelectionSort(ptr,5);
-    /*int minIndex = getMin(ptr,5,0);
-    ptr = updateList(ptr,minIndex,0);
-    cout << ptr[0]<< ptr[1];*/
-    /*cout << lista[getMin(lista, 5, 0)] << endl;
-    cout << lista[getMin(lista, 5, 1)] << endl;
-    cout << lista[getMin(lista, 5, 2)] << endl;
-    cout << lista[getMin(lista, 5, 3)] << endl;
-    cout << lista[getMin(lista, 5, 4)] << endl;*/
-    int minIndex = getMin(ptr,5,0);
-    ptr = updateList(ptr,minIndex,0);
-    int minIndex2 = getMin(ptr,5,1);
-    cout << ptr[minIndex2];
-    ptr = updateList(ptr,minIndex2,1);
-    for (int i = 0; i < 5; i++){
-        cout << ptr[i] << "->";
-    }
-
+    sortBySelectionSort(ptr,11);
     return 0;
 }
