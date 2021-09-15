cbbb681d94023ee5593fbf9a9fc2c33b4210c06a
kpalacios <kpalaciosm02@gmail.com>
2021-09-15 08:39:24 -0600

    Third exercise ready, need to correct an error in exercise 1

--- ThirdExercise.cpp
+++ ThirdExercise.cpp
@@ -3,17 +3,11 @@
 #include <iostream>
 using namespace std;
 
-void sortByInsertionSort(playerList * playerList){
-    playerNode * firstNode = playerList->firstNode;
-    cout << "Primer nodo: " << firstNode->_player->name << " Segundo Nodo: " << firstNode->next->_player->name << endl;
-    cout << firstNode->_player->name.compare(firstNode->next->_player->name) << endl;
-}
-
 bool checkSorted(playerList * playerList){
+    //Checks if the full list is sorted
     playerNode * tmp = playerList->firstNode;
     while (tmp->next != NULL){
-        cout << tmp->_player->name.compare(tmp->next->_player->name) << endl;
-        if (tmp->_player->name.compare(tmp->next->_player->name) < 0)
+        if (tmp->_player->name.compare(tmp->next->_player->name) >= 0)
             return false;
         tmp = tmp->next;
     }
@@ -21,13 +15,14 @@ bool checkSorted(playerList * playerList){
 }
 
 void swapNodes(playerList * playerList, int firstNodeToSwap){
+    //Changes 2 nodes between them. Needs an index to know which node to swap with the next
     playerNode * tmp = playerList->firstNode;
     int actual = 0;
     while (actual != firstNodeToSwap){
         tmp = tmp->next;
         actual++;
     }
-    if (tmp ->next != NULL){
+    if (tmp->next != NULL){
         int idNext = tmp->next->_player->id;
         string nameNext = tmp->next->_player->name;
 
@@ -36,30 +31,54 @@ void swapNodes(playerList * playerList, int firstNodeToSwap){
         
         tmp->_player->id = idNext;
         tmp->_player->name = nameNext;
-    } 
+    }
+}
+
+bool checkNodes(playerList * playerList1, int firstNodeToCheck){
+    //Checks if 2 nodes are sorted between them, does not take into account the other nodes in the list
+    playerNode * tmp = playerList1->firstNode;
+    int actual = 0;
+    while (actual != firstNodeToCheck){
+        tmp = tmp->next;
+        actual++;
+    }
+    if (tmp->next != NULL){
+        if(tmp->_player->name.compare(tmp->next->_player->name) == 1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return true;
+    }
 }
 
 
-void sortByInsertionSort2(playerList * playerList){
-    int large = playerList->getQuantity();
-    while(checkSorted(playerList) != true){
-        for (int i = 0; i <large; i++){
-            swapNodes(playerList,i);
-        }
+void sortByInsertionSort(playerList * playerList1){
+    //Calls the sorting functions until the full list is sorted
+    int large = playerList1->getQuantity();
+    int index = 0;
+    while(checkSorted(playerList1) != true){
+            for (int i = 0; i < large; i++){
+                if (checkNodes(playerList1,i) == false){
+                    swapNodes(playerList1,i);
+                    i = 0;
+                }
+            }
     }
-    playerList->listPlayers();
+
+    playerList1->listPlayers();
 }
 
 int main(){
     playerList * list = new playerList();
-    list->addPlayer(3,"Juan");
-    list->addPlayer(4,"María");
-    list->addPlayer(6,"Pedro");
-    list->addPlayer(8,"Hannah");
-    //sortByInsertionSort(list);
-    //cout << "Sorted list: " << checkSorted(list) << endl;
-    /*list->listPlayers();
-    swapNodes(list,3);
-    list->listPlayers();*/
-    //sortByInsertionSort2(list);
+    list->addPlayer(3,"Hannah");
+    list->addPlayer(4,"Pedro");
+    list->addPlayer(6,"Juan");
+    list->addPlayer(8,"Maria");
+    list->listPlayers();
+    cout << "----Sorting list-----" << endl;
+    sortByInsertionSort(list);
 }
