b2ea3a7c8af28a6b3369e62e136714bac1122798
kpalacios <kpalaciosm02@gmail.com>
2021-09-16 00:24:26 -0600

    There are still bugs in the remove function from the circular double list, need to redo it.

--- ExtraExercise.cpp
+++ ExtraExercise.cpp
@@ -1,10 +1,19 @@
-#include "prueba.hpp"
+#include "doubleCircularPlayerList.hpp"
 int main(){
     doublePlayerList * list = new doublePlayerList();
     list->addPlayer(1,"Juan");
     list->addPlayer(2,"Hannah");
     list->addPlayer(3,"Fabián");
-    cout << "Lenght: " << list->getQuantity() << endl;
-    cout << list->firstNode->next->_player->name << endl;
+    list->addPlayer(4,"Pedro");
+    list->addPlayer(5,"Sabrina");
+    //cout << "Lenght: " << list->getQuantity() << endl;
+    //cout << list->firstNode->previous->next->_player->name << endl;
+    list->listPlayers();
+    list->removePlayer(5);
+    //list->removePlayer(1);
+    list->removePlayer(3);
+    cout << endl << endl;
+    list->listPlayers();
+    //list->addPlayer(6,"Diego");
     list->listPlayers();
 }
---
+++ doubleCircularPlayerList.hpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <iostream>
+using namespace std;
+#include "doublePlayerNode.hpp"
+
+struct doublePlayerList{
+    doublePlayerNode * firstNode;
+    doublePlayerNode * lastNode;
+
+    doublePlayerList(){
+        firstNode = lastNode = NULL;
+    }
+    bool isEmpty(){
+        if (firstNode == NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int getQuantity(){
+        if (isEmpty()){
+            return 0;
+        }
+        else{
+            int length = 0;
+            doublePlayerNode * tmp = firstNode;
+            while(tmp != lastNode){
+                length = length + 1;
+                tmp = tmp->next;
+            }
+            length = length + 1;
+            return length;
+        }
+    }
+    bool addPlayer(int pNumber, string pName){
+        doublePlayerNode * newNode = new doublePlayerNode(pNumber, pName);
+        if (firstNode == NULL){
+            firstNode = lastNode = newNode;
+            return true;
+        }
+        else{
+            newNode->previous = lastNode;
+            newNode->next = firstNode;
+            lastNode->next = newNode;
+            firstNode->previous = newNode;
+            lastNode = newNode;
+            return true;
+        }
+    }
+    bool removePlayer(int pNumber){
+        if (firstNode == NULL){
+            return false;
+        }
+        else{
+            doublePlayerNode * tmp = firstNode;
+            if (lastNode->_player->id == pNumber){
+                lastNode->previous->next = firstNode;
+                firstNode->previous = lastNode->previous;
+                lastNode = lastNode->previous;
+                return true;
+            }
+            else if (firstNode->_player->id == pNumber){
+                firstNode->previous->next = firstNode->next;
+                firstNode->next->previous = firstNode->previous;
+                firstNode = firstNode->next;
+                return true;
+            }
+            else{
+                while(tmp->next != firstNode){
+                    if (tmp->_player->id == pNumber){
+                        tmp->previous->next = tmp->next;
+                        tmp->next->previous = tmp->previous;
+                        tmp = NULL;
+                        return true;
+                    }
+                    else{
+                        tmp = tmp->next;
+                    }
+                }
+                return false;
+            }
+            
+        }
+    }
+    void listPlayers(){
+        if (firstNode == NULL){
+            cout << "FAIL: Tried to print empty list." << endl;
+        }
+        else{
+            doublePlayerNode * tmp = firstNode;
+            while(tmp->next != firstNode){
+                tmp->printData();
+                tmp = tmp->next;
+            }
+            tmp->printData();
+        }
+    }
+};
